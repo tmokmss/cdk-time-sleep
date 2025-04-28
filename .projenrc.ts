b9b519eea27f405e9752cc1e68d6a4207c11f499
Masashi Tomooka <tmokmss@users.noreply.github.com>
2025-04-28 18:45:37 +0900

    chore: upgrade projen (#2)
    * run npx projen upgrade
    
    * use .projenrc.ts
    
    * up

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,19 +1,24 @@
-const { awscdk } = require('projen');
+import { awscdk } from 'projen';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'tmokmss',
   authorAddress: 'tomookam@live.jp',
   cdkVersion: '2.1.0', // For using @aws-cdk/integ-runner
   defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.8.0',
   name: 'cdk-time-sleep',
   repositoryUrl: 'https://github.com/tmokmss/cdk-time-sleep.git',
   eslintOptions: {
+    dirs: [],
     ignorePatterns: ['example/**/*', 'lambda/**/*', 'test/assets/**/*', 'test/*.snapshot/**/*', '*.d.ts'],
   },
-  gitignore: ['*.js', '*.d.ts', '!test/integ.*.snapshot/**/*', "test/cdk.out"],
+  gitignore: ['*.js', '*.d.ts', '!test/integ.*.snapshot/**/*', 'test/cdk.out'],
   keywords: ['aws', 'cdk', 'lambda', 'aws-cdk'],
   tsconfigDev: {
+    compilerOptions: {},
     exclude: ['example', 'test/*.snapshot'],
   },
+  projenrcTs: true,
   devDeps: [
     'aws-cdk@^2.38.0',
     'aws-cdk-lib@^2.38.0',
@@ -26,8 +31,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   description: 'Sleep during deployment (equivalent with Terraform time_sleep)',
 });
+
 // Bundle custom resource handler Lambda code
-project.projectBuild.compileTask.prependExec('yarn install  --frozen-lockfile && yarn build', {
+project.projectBuild.compileTask.prependExec('yarn install --frozen-lockfile && yarn build', {
   cwd: 'lambda',
 });
 // Run integ-test
